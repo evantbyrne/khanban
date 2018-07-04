b70a8b79bcfee3bf06e7b56a96d3693cf45fcb0c
Evan Byrne <evantbyrne@gmail.com>
2018-07-04 18:01:18 -0400

    Basic edit card UI [#9]

--- frontend/js/components/CardDetail.js
+++ frontend/js/components/CardDetail.js
@@ -4,8 +4,52 @@ import { ticketAdd } from '../actions/kanbanActions';
 import Tag from './Tag';
 
 class CardDetail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onCancel = this.onCancel.bind(this);
+    this.onEdit = this.onEdit.bind(this);
+
+    this.state = {
+      is_editing: false,
+    };
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      is_editing: false,
+    });
+  }
+
+  mutate(event, key) {
+    const obj = {};
+    obj[key] = event.target.value;
+    this.setState(obj);
+  }
+
+  onCancel(event) {
+    event.preventDefault();
+
+    this.setState({
+      is_editing: false,
+    });
+  }
+
+  onEdit(event) {
+    event.preventDefault();
+
+    const { card } = this.props;
+
+    this.setState({
+      description: card.description,
+      is_editing: true,
+      title: card.title,
+    });
+  }
+
   render() {
     const { card } = this.props;
+    const mutate = this.mutate.bind(this);
 
     if (card === null) {
       return null;
@@ -13,13 +57,26 @@ class CardDetail extends React.Component {
 
     return (
       <div className="CardDetail">
-        <div className="CardDetail_title">
-          #{card.id} {card.title}
-        </div>
-        {card.description && (
+        {(!this.state.is_editing && (
+          <div className="CardDetail_title">
+            #{card.id} {card.title}
+          </div>
+        )) || (
+          <input className="CardDetail_field"
+            onChange={(e) => mutate(e, 'title')}
+            placeholder="Title..."
+            value={this.state.title} />
+        )}
+        {!this.state.is_editing && card.description && (
           <div className="CardDetail_description">
             {card.description}
           </div>
+        )}
+        {this.state.is_editing && (
+          <textarea className="CardDetail_field -text"
+            onChange={(e) => mutate(e, 'description')}
+            placeholder="Description..."
+            value={this.state.description} />
         )}
         {card.tags.length > 0 && (
           <div className="CardDetail_tags">
@@ -33,8 +90,17 @@ class CardDetail extends React.Component {
             }
           </div>
         )}
-        <button className="CardDetail_button">Edit Card</button>
-        <button className="CardDetail_button -secondary">Archive Card</button>
+        {(!this.state.is_editing && (
+          <React.Fragment>
+            <button className="CardDetail_button" onClick={this.onEdit}>Edit Card</button>
+            <button className="CardDetail_button -secondary">Archive Card</button>
+          </React.Fragment>
+        )) || (
+          <React.Fragment>
+            <button className="CardDetail_button">Save</button>
+            <button className="CardDetail_button -secondary" onClick={this.onCancel}>Cancel</button>
+          </React.Fragment>
+        )}
       </div>
     );
   }
