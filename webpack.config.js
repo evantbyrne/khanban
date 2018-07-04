8e74a1a8390d77daecdaa36d09e3f6b0c8ed1c64
Evan Byrne <evantbyrne@gmail.com>
2018-07-04 13:48:47 -0400

    SCSS compilation [#9]

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
+const MiniCssExtract = require("mini-css-extract-plugin");
 const Uglify = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: {
-    bundle: "./frontend/js/index.js"
+    bundle: [
+      "./frontend/js/index.js",
+      "./frontend/scss/index.scss",
+    ],
   },
 
   output: {
@@ -21,13 +25,32 @@ module.exports = {
         use: [
           {
             loader: "babel-loader"
-          }
-        ]
-      }
-    ]
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtract.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
 
   plugins: [
-    new Uglify()
-  ]
+    new MiniCssExtract({
+      path: path.resolve(__dirname, 'frontend/dist'),
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new Uglify(),
+  ],
 };
